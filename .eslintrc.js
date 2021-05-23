9c04ef8a97fbcb33c32a51d6bdefd138e2581a36
miily8310s <rillakerika@gmail.com>
2021-05-23 12:56:28 +0900

    Add eslint and Editorconfig

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@ota-meshi/svelte/recommended",
+    // MEMO: unavailable @typescript-eslint when svelte-eslint-plugin is used.
+    // "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  // parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  // plugins: ["@typescript-eslint"],
+  rules: {
+    "@ota-meshi/svelte/button-has-type": "error",
+  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  ],
+};
